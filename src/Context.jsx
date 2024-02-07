27528c89260077e694414ba573225ba2702ff760
Ibrahima SOURABIE <ge65yub@mytum.de>
2024-02-07 13:13:47 +0100

    data fetch from API and render

--- src/Context.jsx
+++ src/Context.jsx
@@ -2,8 +2,15 @@ import React, { createContext, useContext, useState } from "react";
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia("(prefers-color-schema:dark)").matches;
+  console.log(prefersDarkMode);
+  return prefersDarkMode;
+};
+
 export const AppProvider = ({ children }) => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("office");
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
@@ -12,7 +19,7 @@ export const AppProvider = ({ children }) => {
     body.classList.toggle("dark-theme", newDarkTheme);
   };
 
-  return <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>{children}</AppContext.Provider>;
+  return <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}>{children}</AppContext.Provider>;
 };
 
 export const useGlobalContext = () => useContext(AppContext);
